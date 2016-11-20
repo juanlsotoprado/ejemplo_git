e5566d3c86d8c5ccee1dac295ed5838a3cd629c4
jsoto <jsoto@leoleo.leoleo>
2016-11-20 16:21:05 +0000

    este es el segundo ejemplo de git

--- ejemplo_git/ejemplos_git/src/ejemplos_git/ejmeplo.java
+++ ejemplo_git/ejemplos_git/src/ejemplos_git/ejmeplo.java
@@ -3,7 +3,8 @@ package ejemplos_git;
 public class ejmeplo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub.     
+		//este es nuevo
 
 	}
 
