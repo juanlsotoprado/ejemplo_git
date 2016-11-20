4acf4663c313a184a62db536fced1df8587a4e2d
jsoto <jsoto@leoleo.leoleo>
2016-11-20 23:42:46 +0000

    werwer

--- ejemplo_git/ejemplos_git/src/ejemplos_git/ejmeplo.java
+++ ejemplo_git/ejemplos_git/src/ejemplos_git/ejmeplo.java
@@ -5,7 +5,7 @@ public class ejmeplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub.     
 		//Git remoto leoleo6
-		//Git local 1
+		//Git local leoleo1
 		//Git remoto 2
 		//Git remoto 3
 
