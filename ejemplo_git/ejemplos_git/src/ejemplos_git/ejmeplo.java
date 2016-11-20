45e1ae9058e1a6fce4a568d502faf2c9b5740663
jsoto <jsoto@leoleo.leoleo>
2016-11-20 23:03:13 +0000

    local 1

--- ejemplo_git/ejemplos_git/src/ejemplos_git/ejmeplo.java
+++ ejemplo_git/ejemplos_git/src/ejemplos_git/ejmeplo.java
@@ -4,8 +4,7 @@ public class ejmeplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub.     
-       //Git 4 desde git hub has un fearch 
-		//Git 4 desde git hub has un fearch 2
+		//Git local 1
 	}
 
 }
