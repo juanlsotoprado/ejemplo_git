e89ab290117e3eec9dfd26930bbae2585b60121d
juanlsotoprado <juanlsotoprado@gmail.com>
2016-11-21 02:36:55 +0000

    Update ejmeplo.java

--- ejemplo_git/ejemplos_git/src/ejemplos_git/ejmeplo.java
+++ ejemplo_git/ejemplos_git/src/ejemplos_git/ejmeplo.java
@@ -4,7 +4,7 @@ public class ejmeplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub.     
-       //Git 4 desde git hub
+       //Git 4 desde git hub has un fearch
 	}
 
 }
