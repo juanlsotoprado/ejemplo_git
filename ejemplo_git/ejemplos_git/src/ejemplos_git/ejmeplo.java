fbf71a85a52c96fb386a96c4cffd0d858e8c9f3a
juanlsotoprado <juanlsotoprado@gmail.com>
2016-11-21 03:01:39 +0000

    Update ejmeplo.java
    se quito el 3

--- ejemplo_git/ejemplos_git/src/ejemplos_git/ejmeplo.java
+++ ejemplo_git/ejemplos_git/src/ejemplos_git/ejmeplo.java
@@ -6,7 +6,6 @@ public class ejmeplo {
 		// TODO Auto-generated method stub.     
        //Git 4 desde git hub has un fearch 
 		//Git 4 desde git hub has un fearch 2
-		//Git 4 desde git hub has un fearch 3
 	}
 
 }
