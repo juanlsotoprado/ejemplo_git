6716a94a32dfbdadd5ade52281fc54004eb3165d
juanlsotoprado <juanlsotoprado@gmail.com>
2016-11-21 03:35:31 +0000

    Update ejmeplo.java

--- ejemplo_git/ejemplos_git/src/ejemplos_git/ejmeplo.java
+++ ejemplo_git/ejemplos_git/src/ejemplos_git/ejmeplo.java
@@ -4,7 +4,7 @@ public class ejmeplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub.     
-		//Git remoto leoleo
+		//Git remoto leoleo2
 		//Git local 1
 		//Git remoto 2
 		//Git remoto 3
